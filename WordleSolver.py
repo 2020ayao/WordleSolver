40e61ad8311ea5d2f5bd6326ae5266292bc204b4
2020ayao <44777427+2020ayao@users.noreply.github.com>
2022-02-15 11:53:18 -0500

    Add files via upload

--- WordleSolver.py
+++ WordleSolver.py
@@ -1,27 +1,48 @@
-def update(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPosition):
+def rank(possible_words, letter_frequencies):
+    scores = {}
+    for word in possible_words:
+        letters = list(word)
+        for letter in letters:
+            if word not in scores.keys():
+                scores[word] = letter_frequencies[letter]
+            else:
+                scores[word] += letter_frequencies[letter]
+    dict(sorted(scores.items(), key=lambda item: item[1]))
+    return scores
+
+def update(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPosition, letter_frequencies):
     for letter in lettersNotInWord:
         possible_words = [word for word in possible_words if letter not in word]
 
     for combo1 in lettersInWord:
-        possible_words = [word for word in possible_words if word[int(combo1[1]) - 1] != combo1[0] and combo1[0] in word]
+        possible_words = [word for word in possible_words if len(word) == 5 and word[int(combo1[1]) - 1] != combo1[0] and combo1[0] in word]
 
     for combo in lettersInWordRightPosition:
-        possible_words = [x for x in possible_words if combo[0] == x[int(combo[1])-1]]
+        possible_words = [x for x in possible_words if len(x) == 5 and combo[0] == x[int(combo[1])-1]]
 
-    print(lettersNotInWord)
-    print(lettersInWord)
-    print(lettersInWordRightPosition)
-    print("Narrowed down to: ", len(possible_words))
-    print(possible_words)
+    # print(lettersNotInWord)
+    # print(lettersInWord)
+    # print(lettersInWordRightPosition)
+    print()
+    print("Narrowed down to: ", len(possible_words), "words.")
+
+    scores = rank(possible_words, letter_frequencies)
+
     if (len(possible_words) > 0):
-        print("Try: ", possible_words[0])
+        word = list(scores.keys())[0]
+        probability = list(scores.values())[0]
+        print("Try: ", word)
+        print("which has a probability of ", probability)
     else:
+        print("No possible words exist")
         exit()
+    if len(possible_words) < 10:
+        print(possible_words)
     return possible_words
 
-def solve(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPosition, possible_guesses):
-    tries = 6
+def solve(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPosition, letter_frequencies):
     while True:
+        print()
         a = input("Letters in Word (a,#) (b,#): ")
         b = input("Letters Not In Word: ")
         c = input("Letters In Word & Right Position (a,#) (b,#): ")
@@ -35,22 +56,29 @@ def solve(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPos
             key = c.split(" ")
             for element1 in key:
                 lettersInWordRightPosition.append([element1[1], element1[3]])
-        possible_words = update(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPosition)
-        tries -= 1
+        possible_words = update(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPosition, letter_frequencies )
 
 def main():
-    possible_guesses = []
+    letter_frequencies = {}
     lettersInWord, lettersNotInWord, lettersInWordRightPosition = [], [], []
     possible_words = []
     c = 0
+    f1 = open('frequency.txt')
+    for line in f1:
+        info = line.split(' ')
+        if len(info) > 1:
+            letter = info[0]
+            frequency = float(info[1])
+            letter_frequencies[letter] = frequency
+    f1.close()
     fh = open('words.txt')
     for line in fh:
-        possible_words.append(line)
+        possible_words.append(line[:-1])
     fh.close()
+    print("Beginning with: ", len(possible_words), "words.")
 
-    print(possible_words)
-    print(len(possible_words))
-    solve(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPosition, possible_guesses)
+    solve(possible_words, lettersInWord, lettersNotInWord, lettersInWordRightPosition, letter_frequencies)
+
 
 if __name__ == "__main__":
     main()
